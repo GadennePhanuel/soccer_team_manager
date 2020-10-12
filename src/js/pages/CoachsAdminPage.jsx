1d12ccc86467c3f0eb5ffba3e2d22f11300d9bc2
Phanuel <phanuelgadenne@yahoo.fr>
2020-10-12 16:24:14 +0200

    requete get coach + affichage tableau + delete coach/id  => OK

--- src/js/pages/CoachsAdminPage.jsx
+++ src/js/pages/CoachsAdminPage.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import Axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import authAPI from '../services/authAPI';
 import usersAPI from '../services/usersAPI';
 
 const CoachAdminPage = (props) => {
     authAPI.setup();
-
     // si role != ROLE_ADMIN -> redirection vers le dashboard qui lui correspond
     const role = usersAPI.checkRole();
     if (role === 'ROLE_COACH') {
@@ -17,9 +17,69 @@ const CoachAdminPage = (props) => {
     if (club === "new") {
         props.history.replace("/createClub/new")
     }
+
+
+    const [coachs, setCoachs] = useState([])
+
+    useEffect(() => {
+        Axios.get('http://localhost:8000/api/coaches')
+            .then(response => response.data['hydra:member'])
+            .then(data => setCoachs(data))
+    })
+
+    
+    const handleDelete = (id) => {
+        console.log(id)
+
+        //copie du tableau original
+        const originalCoachs = [...coachs];
+
+        //supression de l'affichage du coach selectionné
+        setCoachs(coachs.filter((coach) => coach.id !== id));
+
+        Axios.delete("http://localhost:8000/api/coaches/" + id)
+            .then(response => console.log("ok"))
+            .catch(error =>{
+                setCoachs(originalCoachs);
+                console.log(error.response);
+            })
+    }
+
+
+
     return (
         <>
             <h1>Page des coachs pour l'admin</h1>
+
+            <div>
+                <h2>Liste des coachs du club</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Nom</th>
+                            <th>Prénom</th>
+                            <th>Email</th>
+                            <th>phone</th>
+                            <th>action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {coachs.map((coach) => (
+                            <tr key={coach.id}>
+                            <td>{coach.user.lastName}</td>
+                            <td>{coach.user.firstName}</td>
+                            <td>{coach.user.email}</td>
+                            <td>{coach.user.phone}</td>
+                            <td>
+                                <button onClick={() => handleDelete(coach.id)}>
+                                    Supprimer
+                                </button>
+                            </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </>
     );
 }
