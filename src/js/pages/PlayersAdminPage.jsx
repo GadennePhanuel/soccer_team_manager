6dda4f18d0d7ae6189f55f80d273632227d8080c
Phanuel <phanuelgadenne@yahoo.fr>
2020-10-15 10:20:27 +0200

    suppression import axios inutile

--- src/js/pages/PlayersAdminPage.jsx
+++ src/js/pages/PlayersAdminPage.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import Pagination from "../components/Pagination";
 import authAPI from '../services/authAPI';
 import usersAPI from '../services/usersAPI';
