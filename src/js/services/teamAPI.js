a40cb1a55a3939b9dfc0f9b4f254d7b7f1241cda
Phanuel <phanuelgadenne@yahoo.fr>
2020-10-16 11:00:35 +0200

    corection bug merge

--- src/js/services/teamAPI.js
+++ src/js/services/teamAPI.js
@@ -12,18 +12,7 @@ function deleteCoachOnTeam(id) {
     )
 }
 
-function findAllTeams(){
-    return Axios
-            .get("http://localhost:8000/api/teams")
-            .then(response => response.data['hydra:member'])
-}
-
 export default {
     deleteCoachOnTeam,
-<<<<<<< HEAD
-    findAllTeams,
-    
-=======
     findAllTeams
->>>>>>> 1455a634c8d51999595390270daf66a8b44d8ef5
 }
