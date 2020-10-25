cbc0912eaee84372575fd9a7a24d1524a839ff19
Phanu <phanuelgadenne@yahoo.fr>
2020-10-25 18:24:58 +0100

    MyPlayersCoachPage en cours + correction bug sur ProfilForm

--- src/js/pages/MyPlayersCoachPage.jsx
+++ src/js/pages/MyPlayersCoachPage.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import TeamContext from '../contexts/TeamContext';
 import teamAPI from '../services/teamAPI';
 import '../../scss/pages/MyPlayersCoachPage.scss';
+import Axios from 'axios';
 
 const MyPlayersCoachPage = (props) => {
 
@@ -9,37 +10,41 @@ const MyPlayersCoachPage = (props) => {
 
     const [team, setTeam] = useState({})
     const [players, setPlayers] = useState([])
+    const [pictures64, setPictures64] = useState([])
 
     useEffect(() => {
+        setPictures64([])
         //on récupére la team courante
         if (currentTeamId !== '') {
-
             teamAPI.findTeam(currentTeamId)
                 .then(response => {
                     setTeam(response.data)
                     setPlayers(response.data.players)
+
+                    response.data.players.forEach(player => {
+
+                        if (player.picture) {
+                            let id = player.id
+                            Axios.get('http://localhost:8000/api/image/' + player.picture)
+                                .then(response => {
+                                    setPictures64(pictures64 => [...pictures64, { [id]: response.data.data }])
+                                })
+                        }
+                    })
                 })
                 .catch(error => {
                     console.log(error.response)
                 })
+
+            //je récupére toutes les stats
+
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [currentTeamId])
 
-    const [search, setSearch] = useState("");
 
-    const handleSearch = event => {
-        const value = event.currentTarget.value;
-        setSearch(value);
-    }
 
-    const filteredPlayers = players.filter(p =>
-        p.user.firstName.toLowerCase().includes(search.toLowerCase()) ||
-        p.user.lastName.toLowerCase().includes(search.toLowerCase())
-    )
 
-    const handleExclude = (player) => {
-        console.log(player)
-    }
 
     return (
         <div className="wrapper_container MyPlayersCoachPage">
@@ -48,34 +53,38 @@ const MyPlayersCoachPage = (props) => {
                 {currentTeamId === "" ? "Pas d'équipe à charge" : "Joueurs " + team.label + ' ' + team.category}
             </h1>
 
-            <div>
-                <div id="div-search" className="form-group">
-                    <input id="search" type="text" onChange={handleSearch} value={search} className="form-control" placeholder="Rechercher" />
-                </div>
-                <table className="table table-hover">
-                    <thead>
-                        <tr className="thead-color">
-                            <th scope="col">Joueur</th>
-                            <th scope="col">Stats</th>
-                            <th scope="col"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {filteredPlayers.map(player => (
-                            <tr key={player.id}>
-                                <td>{player.user.lastName + ' ' + player.user.firstName}</td>
-                                <td></td>
-                                <td>
-                                    <button className="btn btn-danger" onClick={() => handleExclude(player)}>
-                                        Exclure
-                                    </button>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
+            <div className="cardsDiv">
+                {players.map(player => (
+                    <div className="card" key={player.id}>
 
+                        {player.picture && (
+                            <div className="card-img-top" >
+                                {pictures64.map((picture, index) => (
+                                    <div key={index} className='picture-profil'>
+                                        {picture[player.id] && (
+                                            <img src={`data:image/jpeg;base64,${picture[player.id]}`} alt="" />
+                                        )}
+
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+
+                        {!player.picture && (
+                            <div className="user-picture card-img-top"></div>
+                        )}
+                        <div className="card-body">
+                            <h5 className="card-title">{player.user.lastName + ' ' + player.user.firstName}</h5>
+                            <p className="card-text">Stats</p>
+                            <div className="btns-card">
+                                <button className="btn btn-primary btn-infos">Détails</button>
+                                <button className="btn btn-secondary btn-exclude">Exclure</button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
+
 
         </div>
     );
